388c65ace81f8c09372590bf9eacee785c13b149
nutchak <nutchakirani@gmail.com>
2023-08-01 17:04:36 +0200

    Revert "Revert "make navbar show active link and disable interaction with the active link""
    This reverts commit 633563c97fc2194b11b92fbf979f7e9ef3db234f.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,34 @@ import styles from '../styles/styles.js';
 export default function NavBar() {
 	return (
 		<nav className="absolute w-full z-50 flex items-center justify-between px-6 py-4 font-mono text-2xl sm:px-16 sm:py-12">
-			<NavLink to="/home" className="">
+			<NavLink 
+                to="/home"
+                className={({isActive: isRouteActive}) => (isRouteActive ? styles.disabledLink : '')}
+            >
 				Home
 			</NavLink>
-			<NavLink to="/education" className="">
+			<NavLink 
+                to="/education"
+                className={({isActive: isRouteActive}) => (isRouteActive ? styles.disabledLink : '')}
+            >
 				Education
 			</NavLink>
-			<NavLink to="/projects" className="">
+			<NavLink 
+                to="/projects"
+                className={({isActive: isRouteActive}) => (isRouteActive ? styles.disabledLink : '')}
+            >
 				Projects
 			</NavLink>
-			<NavLink to="/about" className="">
+			<NavLink 
+                to="/about"
+                className={({isActive: isRouteActive}) => (isRouteActive ? styles.disabledLink : '')}
+            >
 				About
 			</NavLink>
-			<NavLink to="/contact" className="">
+			<NavLink 
+                to="/contact"
+                className={({isActive: isRouteActive}) => (isRouteActive ? styles.disabledLink : '')}
+            >
 				Contact
 			</NavLink>
 		</nav>
